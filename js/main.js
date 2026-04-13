dbea74757fa079a3616dda0fd0fab2895a8bc05f
Shoko Haatveit <shoko@haatveit.org>
2026-04-13 14:25:18 +0200

    navbar animation

--- js/main.js
+++ js/main.js
@@ -3,18 +3,25 @@ gsap.from(".hero__title", {
     y: 50,
     opacity: 0,
     delay: 0.3,
-})
+});
 
 gsap.from(".hero__subtitle", {
     duration: 1,
     y: 50,
     opacity:0,
     delay:0.6,
-})
+});
 
 gsap.from(".hero__btn", {
     duration:1,
     y: 50,
     opacity:0,
     delay: 0.9,
-})
+});
+
+gsap.from(".navbar", {
+    duration: 1,
+    y: -50,
+    opacity: 0,
+    delay: 0.1,
+});
