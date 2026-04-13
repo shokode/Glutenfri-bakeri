9fe2abeae476172008dbf66c7c796df536a90b34
Shoko Haatveit <shoko@haatveit.org>
2026-04-13 14:16:26 +0200

    initial comit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+gsap.from(".hero__title", {
+    duration: 1,
+    y: 50,
+    opacity: 0,
+    delay: 0.3,
+})
+
+gsap.from(".hero__subtitle", {
+    duration: 1,
+    y: 50,
+    opacity:0,
+    delay:0.6,
+})
+
+gsap.from(".hero__btn", {
+    duration:1,
+    y: 50,
+    opacity:0,
+    delay: 0.9,
+})
